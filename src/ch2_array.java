e29194a36bf36b15225111c5c46db0b07a6d2fcd
bibi6666667 <bibi6666667@kakao.com>
2020-11-28 09:05:26 +0900

    [챕터 2] 선택정렬 자바 버전(ing)
    자바 선택정렬 코드를 구글링하며 공부.
    - 아직 4.번 부분이 이해되지 않았다!

--- src/ch2_array.java
+++ src/ch2_array.java
@@ -14,34 +14,50 @@ public class ch2_array {
 
     public static void main(String[] args) {
         ch2_array array = new ch2_array();
-        int[] result = array.selectionSort(array.arrInput);
-        System.out.println(Arrays.toString(result));
+        array.selectionSortJava(array.arrInput);
+        // findSmallest(), selectionSort()는 동작하지 않음.
+        //
     }
 
-    int findSmallest(int[] arr){ // 배열에서 가장 작은 정수 찾기
+    int findSmallest(int[] arr) { // 배열에서 가장 작은 정수 찾기
         smallest = arr[0]; // 가장 작은 정수 저장. 초기값으로 첫번째 원소 저장
         smallest_index = 0; // smallest의 인덱스 저장
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] < smallest) { // 배열의 원소가 smallest보다 작으면
                 smallest = arr[i]; // 그 원소가 smallest가 되고,
                 smallest_index = i; // 그 원소의 인덱스가 smallest_index가 된다
-                // 잘 동작하지 않는다.
                 // 자바에는 .append()와 .pop()메서드가 기본제공되지 않기 때문에,
                 // 최솟값을 찾아도 다음 탐색 때 그 최솟값을 빼고 나머지 값에서 탐색할 수가 없다
                 // - stack패키지를 임포트하거나
                 // - 현재 메서드 구조를 부수고 이중for문으로 작성해야 자바에서 선택정렬을 구현할 수 있다..
-                // TODO : 어떻게든 방법을 찾아 자바에서 선택정렬 구현하기. - 구글링 또는 임포트
-
             }
         }
         return smallest_index;
     }
 
     int[] selectionSort(int[] arrInput) { // 찾은 가장 작은 정수를 새 배열에 넣기
-        for(int i = 0;  i < arrInputLength; i++) {
+        for (int i = 0; i < arrInputLength; i++) {
             smallest = arrInput[findSmallest(arrInput)]; // 배열에서 가장 작은 정수를 찾기
             newArr[i] = smallest; // 가장 작은 정수를 새 배열에 추가
         }
         return newArr;
     }
+
+    void selectionSortJava(int[] arr) {
+        int indexMin, temp;
+        for (int i = 0; i < arr.length - 1; i++) { // 1. 비교할 원소의 인덱스 선택 (0번째부터 시작)
+            indexMin = i;
+            for (int j = i + 1; j < arr.length; j++) { // 2. i+1번째 원소부터 1.과 비교 시작
+                if (arr[j] < arr[indexMin]) { // 3. (오름차순) 비교할 원소보다 i+1번째 원소가 작다면, 위치를 갱신
+                    indexMin = j;
+                }
+            }
+            // 4. swap(arr[indexMin], arr[i])
+            temp = arr[indexMin]; //
+            arr[indexMin] = arr[i]; //
+            arr[i] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
 }
